a2e7ac3d15cc20494bf5ec9e0da2fc5dff15bc0a
felipejoq <felipejofreq@gmail.com>
2023-11-09 17:40:10 -0300

    Modificación del archivo README.md

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="text-center mt-5 mb-5 container bg-light rounded shadow-sm p-2">
       <p className="fs-6">
         <i className="bi bi-braces"></i> Desarrollado por <a href="https://uncodigo.com/" target="_blank" rel="noreferrer" className="text-black text-muted">Felipe JQ</a> <br />
-        <i className="bi bi-github"></i> <a href="#" target="_blank" rel="noreferrer" className="text-black text-muted">Repositorio</a>
+        <i className="bi bi-github"></i> <a href="https://github.com/felipejoq/practica-II-react-router" target="_blank" rel="noreferrer" className="text-black text-muted">Repositorio</a>
       </p>
     </footer>
   );
